3f00ed7435dafd3c145f884f8c95804b301554c0
Rostam Shirani <rostams@lyft.com>
2019-12-26 14:40:16 -0800

    Add two specific different tags to dockerhub

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 # Build the images
-docker build -t rostam63/multi-client -f ./client/Dockerfile ./client
-docker build -t rostam63/multi-server -f ./server/Dockerfile ./server
-docker build -t rostam63/multi-worker -f ./worker/Dockerfile ./worker
+# latest tag used in dev environment, $SHA tag gaurantees uniqueness in Prod environment
+docker build -t rostam63/multi-client:latest -t rostam63/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rostam63/multi-server:latest -t rostam63/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rostam63/multi-worker:latest -t rostam63/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # Build the images
-docker push rostam63/multi-client
-docker push rostam63/multi-server
-docker push rostam63/multi-worker
+docker push rostam63/multi-client:latest
+docker push rostam63/multi-server:latest
+docker push rostam63/multi-worker:latest
+
+docker push rostam63/multi-client:$SHA
+docker push rostam63/multi-server:$SHA
+docker push rostam63/multi-worker:$SHA
 
 # K8S apply
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=rostam63/multi-server
+kubectl set image deployments/client-deployment client=rostam63/multi-client:$SHA
+kubectl set image deployments/server-deployment server=rostam63/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=rostam63/multi-worker:$SHA
